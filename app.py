914a0ee1eeafc48b247313071e8c5eb99ab1ac05
Infouktour3 1 <infouktour@gmail.com>
2024-01-09 01:22:18 +0000

    updated

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    review = db.Column(db.Text, nullable=True)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
+
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    title = request.form['title']
+    author = request.form['author']
+    review = request.form['review']
+
+    new_book = Book(title=title, author=author, review=review)
+    db.session.add(new_book)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
