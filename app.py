363c05fe169e297ff923faf370a67dbb34275f55
Infouktour3 1 <infouktour@gmail.com>
2024-01-09 01:42:45 +0000

    updated files that disappeared yesterday

--- app.py
+++ app.py
@@ -1,33 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+import os
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
-db = SQLAlchemy(app)
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(50), nullable=False)
-    review = db.Column(db.Text, nullable=True)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_default_secret_key')
 
 @app.route('/')
-def index():
-    books = Book.query.all()
-    return render_template('index.html', books=books)
-
-@app.route('/add_book', methods=['POST'])
-def add_book():
-    title = request.form['title']
-    author = request.form['author']
-    review = request.form['review']
-
-    new_book = Book(title=title, author=author, review=review)
-    db.session.add(new_book)
-    db.session.commit()
-
-    return redirect(url_for('index'))
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,5 @@
+// static/script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document ready!'); // Add your JavaScript code here
+});
